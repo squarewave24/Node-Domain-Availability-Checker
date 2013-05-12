4423e66f506c46f9040f80c41a5bc4c56affa53d
Rafal <sonicsoul@gmail.com>
2013-05-12 14:36:01 -0400

    cleanup

--- run.js
+++ run.js
@@ -31,8 +31,6 @@ mgr.checkDomain(domains, 20, false);
 
 
 
-
-
 function GenerateDomainNames(limit) {
     var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'z'];
     var domains = [];
